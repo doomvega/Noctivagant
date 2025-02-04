a4195f00fd9659b631af61548fd54e24c97cc310
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-02-04 17:07:22 -0500

    Changes to the foxwidget styling

--- public/foxring/onionring-widget.js
+++ public/foxring/onionring-widget.js
@@ -58,12 +58,12 @@ else {
   tag.insertAdjacentHTML('afterbegin', `
   <table>
     <tr>
-      <td class='webring-prev'><a href='${sites[previousIndex]}'>← previous</a></td>
-      <td class='webring-info'>This site is part of the ${ringName} webring</br>
+      <td class='webring-prev'><a href='${sites[previousIndex]}'>← prev</a></td>
+      <td class='webring-info'>🔗 ${ringName} Foxring 🦊</br>
       <span class='webring-links'>
         ${randomText}
         ${indexText}
-        <a href='https://garlic.garden/onionring/'>what is this?</a></span></td>
+        <a href='https://garlic.garden/onionring/'>Made with Onionring!</a></span></td>
       <td class='webring-next'><a href='${sites[nextIndex]}'>next →</a></td>
     </tr>
   </table>
