79f72027dcc4b5fe2b26e7e102ca162f54373593
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-02-04 17:00:24 -0500

    Did I forget to save the onionring vars?

--- public/foxring/onionring-variables.js
+++ public/foxring/onionring-variables.js
@@ -7,19 +7,16 @@
 
 //the full URLs of all the sites in the ring
 var sites = [
-'https://example.com/',
-'https://example.com/',
-'https://example.com/',
-'https://example.com/'
+'https://noctivagant.net/',
 ];
 
 //the name of the ring
-var ringName = 'My Ring';
+var ringName = 'Vulpesites';
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'my-ring';
+var ringID = 'vulpesites';
 
 //should the widget include a link to an index page?
 var useIndex = false;
