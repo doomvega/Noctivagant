98f9871bc41bc6d9a4cbb94136243491982067d1
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-02-04 20:37:36 -0500

    Add header back to blog?

--- public/JS/headNavStatic.js
+++ public/JS/headNavStatic.js
@@ -14,3 +14,21 @@ const headNav = `
 ` ;
 
     document.getElementById("headNav").innerHTML = headNav
+
+const headNav2 = `
+    <h1>DOOMVEGA's Poasts</h1>
+    <a class="navButton" href="https://noctivagant.net/">home&nbsp;🦊</a>
+    <a class="navButton" href="/shrine/interests">shrines&nbsp;❤️</a>
+    <a class="navButton" href="/creations/music">music&nbsp;🎶</a>
+    <a class="navButton" href="/characters/overview">characters&nbsp;💫</a>
+    <a class="navButton" href="/self/socials">socials&nbsp;💬</a>
+    <a class="navButton" href="/self/projects">projects&nbsp;🏆</a>
+    <a class="navButton" href="/self/now">now&nbsp;✨️</a>
+    <a class="navButton" href="/links/wall">links&nbsp;🌐</a>
+    <a class="navButton" href="/links/webrings">webrings&nbsp;🤝</a>
+    <a class="navButton" href="/meta/updates">updates&nbsp;💻</a>
+    <a class="navButton" href="/meta/sitemap">sitemap&nbsp;🗺️</a>
+    <a class="navButton" href="/thanks">thanks&nbsp;⭐️</a>
+` ;
+
+    document.getElementById("headNav2").innerHTML = headNav2
