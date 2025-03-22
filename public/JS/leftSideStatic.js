e82993808364a9e0d21aa18b2090a77d32549b17
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-03-22 00:05:07 -0400

    Discord iframe attempt in lefthand bar

--- public/JS/leftSideStatic.js
+++ public/JS/leftSideStatic.js
@@ -1,13 +1,13 @@
 const leftSide = ` 
-<p>Atlas, Vega, or Jupi! Welcome to my website! This is where I gather and talk about the things I love. Looking for my commission information? <a href="https://doomvega.art">You can find it here!</a></p>
+<p>Atlas, Vega, or Jupi! Welcome to my website! This is where I gather and talk about the things I love.</p>
 <hr>
 <center><p id="blink" style="font-size: large;">🎨 HEY! ⭐</p></center>
 <p>If you like art made by human hands (or fox paws), check out my <a href="https://doomvega.art/">commissions website!</a> It means the world to me ❤️🦊</p>
 <hr>
-<p>If you enjoy my site, please bookmark it. I also encourage you to create your own sites in order to share your passions with the world!</p>
-<hr>
 <center><a href="http://tamanotchi.world/2881c"><img src="http://tamanotchi.world/i/2881" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/5554c"><img src="https://tamanotchi.world/i/5554" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/3373c"><img src="https://tamanotchi.world/i/3373" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/21096c"><img src="https://tamanotchi.world/i/21096" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/20873c"><img src="https://tamanotchi.world/i/20873" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/21100c"><img src="https://tamanotchi.world/i/21100" alt="It's tamaNOTchi! Click to feed!"></a></center>
 <p>Click here to feed my Tamanotchis! :)</p>
+<hr>
+<iframe src="https://discord.com/widget?id=1129560255936872449&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 ` ;
 
     document.getElementById("leftSide").innerHTML = leftSide
