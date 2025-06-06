d419fc459e0588bd8c3c2ec9c252e2ee0c6bbcff
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-06-06 02:33:53 -0400

    One final sidebar change for the night; change smaller tag to i tag

--- public/JS/leftSideStatic.js
+++ public/JS/leftSideStatic.js
@@ -10,7 +10,7 @@ const leftSide = `
 <center><a href="https://dopcom.net/creechers/p/room/6hg_ACPi"><img src="https://dopcom.net/creechers/p/pet/6hg_ACPi"></a><a href="https://dopcom.net/creechers/p/room/Pk8FACPi"><img src="https://dopcom.net/creechers/p/pet/Pk8FACPi"></a><a href="https://dopcom.net/creechers/p/room/L5tMACSd"><img src="https://dopcom.net/creechers/p/pet/L5tMACSd"></a></center>
 <p>PLAY <a href="https://dopcom.net/creechers/p/">DOPCOM CREECHERS</a></p>
 <h3>Join our Discord!</h3>
-<p><smaller>You can find more information on our server on the <a href="/self/socials.html">socials</a> page.</smaller></p>
+<p><i>You can find more information on our server on the <a href="/self/socials.html">socials</a> page.</i></p>
 <iframe src="https://discord.com/widget?id=1129560255936872449&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 ` ;
 
