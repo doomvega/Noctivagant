3f4148068e2f72e839062948c82731c15202e5af
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-06-06 02:24:43 -0400

    Test Dopcom embed

--- public/JS/leftSideStatic.js
+++ public/JS/leftSideStatic.js
@@ -7,6 +7,8 @@ const leftSide = `
 <center><a href="http://tamanotchi.world/2881c"><img src="http://tamanotchi.world/i/2881" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/5554c"><img src="https://tamanotchi.world/i/5554" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/3373c"><img src="https://tamanotchi.world/i/3373" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/21096c"><img src="https://tamanotchi.world/i/21096" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/20873c"><img src="https://tamanotchi.world/i/20873" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/21100c"><img src="https://tamanotchi.world/i/21100" alt="It's tamaNOTchi! Click to feed!"></a></center>
 <p>Click here to feed my Tamanotchis! :)</p>
 <hr>
+<a href="https://dopcom.net/creechers/p/rrom/6hg_ACPi"><img src="https://dopcom.net/creechers/p/pet/6hg_ACPi"></a>
+<hr>
 <iframe src="https://discord.com/widget?id=1129560255936872449&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 ` ;
 
