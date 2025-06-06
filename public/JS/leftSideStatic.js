f7b63faeefd637fb0aef6ac7c1978aef6d06be96
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-06-06 02:29:34 -0400

    Update left sidebar with more information

--- public/JS/leftSideStatic.js
+++ public/JS/leftSideStatic.js
@@ -4,11 +4,14 @@ const leftSide = `
 <center><p id="blink" style="font-size: large;">🎨 HEY! ⭐</p></center>
 <p>If you like art made by human hands (or fox paws), check out my <a href="https://doomvega.art/">commissions website!</a> It means the world to me ❤️🦊</p>
 <hr>
+<h2>My Tamanotchis</h2>
 <center><a href="http://tamanotchi.world/2881c"><img src="http://tamanotchi.world/i/2881" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/5554c"><img src="https://tamanotchi.world/i/5554" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/3373c"><img src="https://tamanotchi.world/i/3373" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/21096c"><img src="https://tamanotchi.world/i/21096" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/20873c"><img src="https://tamanotchi.world/i/20873" alt="It's tamaNOTchi! Click to feed!"></a><a href="https://tamanotchi.world/21100c"><img src="https://tamanotchi.world/i/21100" alt="It's tamaNOTchi! Click to feed!"></a></center>
-<p>Click here to feed my Tamanotchis! :)</p>
 <hr>
-<a href="https://dopcom.net/creechers/p/rrom/6hg_ACPi"><img src="https://dopcom.net/creechers/p/pet/6hg_ACPi"></a>
+<h2>My Creechers</h2>
+<a href="https://dopcom.net/creechers/p/room/6hg_ACPi"><img src="https://dopcom.net/creechers/p/pet/6hg_ACPi"></a><a href="https://dopcom.net/creechers/p/room/Pk8FACPi"><img src="https://dopcom.net/creechers/p/pet/Pk8FACPi"></a><a href="https://dopcom.net/creechers/p/room/L5tMACSd"><img src="https://dopcom.net/creechers/p/pet/L5tMACSd"></a>
 <hr>
+<h2>Join our Discord!</h2>
+<p><smaller>You can find more information on our server on the <a href="/self/socials.html">socials</a> page.</smaller></p>
 <iframe src="https://discord.com/widget?id=1129560255936872449&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 ` ;
 
