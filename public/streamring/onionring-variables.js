07e91b6bbd2988a1b93868a34a7322f977ae71f2
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-06-10 22:24:23 -0400

    Add streamring index page; update streamring widget

--- public/streamring/onionring-variables.js
+++ public/streamring/onionring-variables.js
@@ -21,9 +21,9 @@ var ringName = 'Euonymy';
 var ringID = 'euonymstreamring';
 
 //should the widget include a link to an index page?
-var useIndex = false;
+var useIndex = true;
 //the full URL of the index page. if you're not using one, you don't have to specify anything here
-var indexPage = 'https://example.com/index.html';
+var indexPage = 'https://noctivagant.net/links/euonymy.html';
 
 //should the widget include a random button?
 var useRandom = true;
