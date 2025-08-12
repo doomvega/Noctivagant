eb9ab42b75d486227eac7541713a944829f8324f
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-08-12 15:42:22 -0400

    yeah I updated the left bar. hell yes. DO NOT BREAK. this is my blessing

--- public/JS/leftSideStatic.js
+++ public/JS/leftSideStatic.js
@@ -12,6 +12,8 @@ const leftSide = `
 <h3>My Creechers</h3>
 <center><a href="https://dopcom.net/creechers/p/room/6hg_ACPi"><img src="https://dopcom.net/creechers/p/pet/6hg_ACPi"></a><a href="https://dopcom.net/creechers/p/room/Pk8FACPi"><img src="https://dopcom.net/creechers/p/pet/Pk8FACPi"></a><a href="https://dopcom.net/creechers/p/room/L5tMACSd"><img src="https://dopcom.net/creechers/p/pet/L5tMACSd"></a></center>
 <p>PLAY <a href="https://dopcom.net/creechers/p/">DOPCOM CREECHERS</a></p>
+<hr>
+<div id="wcb" class="carbonbadge"></div>
 <h3>Join our Discord!</h3>
 <p><i>You can find more information regarding our server on the <a href="/self/socials.html">socials</a> page.</i></p>
 <iframe src="https://discord.com/widget?id=1129560255936872449&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
