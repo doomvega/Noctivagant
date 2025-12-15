d8c61bde7614c0e79f8d474c92ea1030994e2f4f
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2025-12-15 01:02:29 -0500

    change Streamring, prepare for removal

--- public/streamring/onionring-variables.js
+++ public/streamring/onionring-variables.js
@@ -8,8 +8,6 @@
 //the full URLs of all the sites in the ring
 var sites = [
 'https://noctivagant.net/',
-'https://dopcom.net/',
-'https://brisken.neocities.org/'
 ];
 
 //the name of the ring
@@ -18,7 +16,7 @@ var ringName = 'Euonymy';
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'euonymstreamring';
+var ringID = 'euonymstreamringOld';
 
 //should the widget include a link to an index page?
 var useIndex = true;
