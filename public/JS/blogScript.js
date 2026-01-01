f96b02b276bcaa9e1aa626dbd150c27168875da2
Atlas/Vega/Jupi <152674429+doomvega@users.noreply.github.com>
2026-01-01 00:59:59 -0500

    New blog post

--- public/JS/blogScript.js
+++ public/JS/blogScript.js
@@ -41,6 +41,7 @@ let postsArray = [
 //[ "posts/2020-11-10-My-Third-Post-Example.html", "" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html", "" ],
 //[ "posts/2020-11-10-Post-Template.html", "", "exampleTag1", "exampleTag2" ],
+[ "posts/2026-01-01-Instantiate-Change.html", "", "vega", "politics", "2026", "goals" ],
 [ "posts/2025-10-02-We-Did-It.html", "", "vega", ],
 [ "posts/2025-07-01-If-I-Die.html", "", "vega", "poetry" ],
 [ "posts/2025-03-25-Trans-Hope.html", "", "vega", "trans" ],
